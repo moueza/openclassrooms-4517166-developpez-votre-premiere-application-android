ca415d0994fd7d84c7ee792a3475e54d1d63e10b
moueza <>
2020-10-02 08:36:32 +0200

    Resolves #5 P1C4 Référencez les éléments graphiques dans votre activité #5 ; see #1 gen : run Gradle OK

--- app/src/main/java/com/ponroy/florian/topquiz/MainActivity.java
+++ app/src/main/java/com/ponroy/florian/topquiz/MainActivity.java
@@ -2,14 +2,25 @@
 package com.ponroy.florian.topquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.homniserv.moueza.peter.topquiz.R;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
     }
 }
