726d0ab171967c4129dcf96d70b262bd279cbef4
moueza <>
2020-10-03 12:16:39 +0200

    Resolves #11 P2C1 Créez une seconde activite ; see #1 gen Emu OK

---
+++ app/src/main/java/mypack/GameActivity.java
@@ -0,0 +1,30 @@
+package mypack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+    private TextView mQuestionText;
+    private Button mAnswerButton1;
+    private Button mAnswerButton2;
+    private Button mAnswerButton3;
+    private Button mAnswerButton4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+
+        mQuestionText = (TextView) findViewById(R.id.activity_game_question_text);
+         mAnswerButton1 = (Button) findViewById(R.id.activity_game_answer1_btn);
+        mAnswerButton2 = (Button) findViewById(R.id.activity_game_answer2_btn);
+        mAnswerButton3 = (Button) findViewById(R.id.activity_game_answer3_btn);
+        mAnswerButton4 = (Button) findViewById(R.id.activity_game_answer4_btn);
+
+    }
+}
--- app/src/main/java/mypack/MainActivity.java
+++ app/src/main/java/mypack/MainActivity.java
@@ -1,5 +1,6 @@
 package mypack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -49,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // The user just clicked
-            }
+                Intent gameActivity = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameActivity);            }
         });
     }
 }
