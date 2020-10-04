5ff714e6e53a7caa2b06b27fc6d8cb72b9cda78b
moueza <>
2020-10-04 12:33:29 +0200

    Resolves #12 P2C2 Découvrez l'architecture Modèle-Vue-Contrôleur

--- app/src/main/java/mypack/controller/GameActivity.java
+++ app/src/main/java/mypack/controller/GameActivity.java
@@ -1,10 +1,12 @@
-package mypack;
+package mypack.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import mypack.R;
 
 public class GameActivity extends AppCompatActivity {
     private TextView mQuestionText;
