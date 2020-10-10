c2e67d64a5541776090e53f83f0001e2ec1835d0
moueza <>
2020-10-10 19:01:24 +0200

    my impl ; see #1 gen : simu OK

---
+++ app/src/main/java/mypack/model/Item.java
@@ -0,0 +1,14 @@
+package mypack.model;
+
+/**
+ * Created by Peter MOUEZA on 05/10/20 06:20
+ *  
+ */
+public class Item {
+    int order;
+   float valueRandom;
+   Question quest;
+
+    public Item() {
+    }
+}
