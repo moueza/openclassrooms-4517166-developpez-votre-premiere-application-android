4cf8bca65c2a0cc7eb54bee215eac58d3581083f
moueza <>
2020-12-25 11:11:15 +0100

    sov

---
+++ app/build/generated/source/buildConfig/release/mypack/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mypack;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.homniserv.moueza.peter.topquiz";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
