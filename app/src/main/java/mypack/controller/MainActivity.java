237698dd86ca6e335c2cd66543bcc045edcf51bf
moueza <>
2020-12-29 06:14:42 +0100

    see #47 prof

--- app/src/main/java/mypack/controller/MainActivity.java
+++ app/src/main/java/mypack/controller/MainActivity.java
@@ -1,111 +1,144 @@
-package mypack.controller;
+package com.ponroy.florian.topquiz.controller;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import mypack.R;
-import mypack.model.User;
+import com.ponroy.florian.topquiz.R;
+import com.ponroy.florian.topquiz.model.User;
+
+import static java.lang.System.out;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
-    public static final String PREF_KEY_FIRSTNAME = "PREF_KEY_FIRSTNAME";
-    private static final int GAME_ACTIVITY_REQUEST_CODE = 42;
-    private static final String TAG = "MainActivity";
+
     private TextView mGreetingText;
-    private TextView scoreText;
     private EditText mNameInput;
     private Button mPlayButton;
     private User mUser;
+    public static final int GAME_ACTIVITY_REQUEST_CODE = 42;
     private SharedPreferences mPreferences;
+
+    public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
+    public static final String PREF_KEY_FIRSTNAME = "PREF_KEY_FIRSTNAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-        Log.e("fromm", "lbl10");
         super.onCreate(savedInstanceState);
-        Log.e("fromm", "log de Peter MOUEZA");
         setContentView(R.layout.activity_main);
 
+        System.out.println("MainActivity::onCreate()");
 
         mUser = new User();
+
         mPreferences = getPreferences(MODE_PRIVATE);
 
         mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
-        if (null!=getPreferences(MODE_PRIVATE).getString(PREF_KEY_FIRSTNAME,null)){
-            mNameInput.setEnabled(false);
-        }else{
-            mNameInput.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    // This is where we'll check the user input
-                    mPlayButton.setEnabled(s.toString().length() != 0);
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            });
-        }
-
-        if (null!=getPreferences(MODE_PRIVATE).getString(PREF_KEY_SCORE,null)){
-           scoreText.setText(getPreferences(MODE_PRIVATE).getString(PREF_KEY_SCORE,null));
-        }else{
-            Log.e(TAG,"lbl444 No score");
-        }
-
-
         mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
 
         mPlayButton.setEnabled(false);
-        scoreText = (TextView) findViewById(R.id.score_txt);
+
+        greetUser();
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPlayButton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String firstname = mNameInput.getText().toString();
-                mUser.setFirstName(firstname);
+                mUser.setFirstname(firstname);
 
-                mPreferences.edit().putString(PREF_KEY_FIRSTNAME, mUser.getFirstName()).apply();
+                mPreferences.edit().putString(PREF_KEY_FIRSTNAME, mUser.getFirstname()).apply();
 
-                Intent gameActivity = new Intent(MainActivity.this, GameActivity.class);
-                //startActivity(gameActivity);
-                startActivityForResult(gameActivity, GAME_ACTIVITY_REQUEST_CODE);
+                // User clicked the button
+                Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivityForResult(gameActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
             }
         });
     }
 
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (GAME_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK == resultCode) {
             // Fetch the score from the Intent
             int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
-            Log.e(TAG, "erreur=" + score);
-            // scoreText.setText(score); cause crash
 
             mPreferences.edit().putInt(PREF_KEY_SCORE, score).apply();
 
-        }else{
-            Log.e(TAG, "NULL1" );
+            greetUser();
         }
+    }
+
+    private void greetUser() {
+        String firstname = mPreferences.getString(PREF_KEY_FIRSTNAME, null);
+
+        if (null != firstname) {
+            int score = mPreferences.getInt(PREF_KEY_SCORE, 0);
+
+            String fulltext = "Welcome back, " + firstname
+                    + "!\nYour last score was " + score
+                    + ", will you do better this time?";
+            mGreetingText.setText(fulltext);
+            mNameInput.setText(firstname);
+            mNameInput.setSelection(firstname.length());
+            mPlayButton.setEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        out.println("MainActivity::onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        out.println("MainActivity::onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        out.println("MainActivity::onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        out.println("MainActivity::onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        out.println("MainActivity::onDestroy()");
     }
 }
